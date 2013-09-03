56938a7ef3e3bd797939a95b138a90bfe7d3f60c
Thbego <gui@gumatoMacBook-Air.local>
2013-09-03 20:43:18 +0800

    homework
    helloworld

---
+++ hello world/hello world/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Thbego on 13-9-3.
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!I'm Thbego!\n";
+    return 0;
+}
+
